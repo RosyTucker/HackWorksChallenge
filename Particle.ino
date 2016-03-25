325016d3ab63df59181ff258831638824eed8b33
Rosy Tucker <rtucker@thoughtworks.com>
2016-03-25 18:58:08 +0000

    Parse task destination

--- Particle.ino
+++ Particle.ino
@@ -9,6 +9,9 @@ void setup() {
 void loop() {
  String response = webClient.get("/task");
   String body = getResponseBody(response);
+  String destination = getDestinationFromBody(body);
+
+  Serial.println("Recieved Task for: " + destination);
 
   delay(5000);
 }
@@ -17,4 +20,11 @@ String getResponseBody (String response) {
   int bodyStartIndex = response.indexOf("\n\r") + 4;
   response.remove(0, bodyStartIndex);
   return response;
+}
+
+String getDestinationFromBody (String response) {
+  String searchTerm = "\"destination\":\"";
+  int firstDestinationIndex = response.lastIndexOf(searchTerm) + searchTerm.length();
+  response.remove(0, firstDestinationIndex);
+  return response.substring(0, 3);
 }
