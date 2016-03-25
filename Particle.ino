4d1cdaa364b7c95e21c4c44f9e360c92cf0065fd
Rosy Tucker <rtucker@thoughtworks.com>
2016-03-25 20:01:38 +0000

    Add type parsing

--- Particle.ino
+++ Particle.ino
@@ -2,18 +2,37 @@
 
 WebClient webClient = WebClient("hackworks-challenge.herokuapp.com");
 
+// Declare any fields here
+
 void setup() {
   Serial.println("Starting...");
+
+  // Insert any setup code here
 }
 
 void loop() {
- String response = webClient.get("/task");
+  fetchTask();
+  delay(5000);
+}
+
+void performTask(String destination, int type) {
+    // Insert code to perform tasks in here
+}
+
+
+/*********************************************************************
+ *   Helper functions for response parsing and making the HTTP call  *
+ *********************************************************************/
+
+void fetchTask() {
+  String response = webClient.get("/task");
   String body = getResponseBody(response);
   String destination = getDestinationFromBody(body);
+  int type = getTypeFromBody(body);
 
-  Serial.println("Recieved Task for: " + destination);
+  Serial.println("Recieved Task for: " + destination + " with type: " + type);
 
-  delay(5000);
+  performTask(destination, type);
 }
 
 String getResponseBody (String response) {
@@ -22,9 +41,20 @@ String getResponseBody (String response) {
   return response;
 }
 
-String getDestinationFromBody (String response) {
+String getDestinationFromBody (String body) {
   String searchTerm = "\"destination\":\"";
-  int firstDestinationIndex = response.lastIndexOf(searchTerm) + searchTerm.length();
-  response.remove(0, firstDestinationIndex);
-  return response.substring(0, 3);
+  int firstDestinationIndex = body.indexOf(searchTerm) + searchTerm.length();
+  body.remove(0, firstDestinationIndex);
+  return body.substring(0, 3);
+}
+
+int getTypeFromBody (String body) {
+  String searchTerm = "\"type\":";
+  int firstTypeIndex = body.indexOf(searchTerm) + searchTerm.length();
+  body.remove(0, firstTypeIndex);
+
+  int i = 0;
+  while(body.charAt(i) >= '0' && body.charAt(i) <= '9' ) i++;
+
+  return body.substring(0,i).toInt();
 }
